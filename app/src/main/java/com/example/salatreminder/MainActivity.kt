c184f64e8d03b0b13e7b64e4180c23f10e904b1d
amr._.dev <nasseramraoui2004@gmail.com>
2024-11-07 20:40:08 +0100

    first commit

---
+++ app/src/main/java/com/example/salatreminder/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.salatreminder
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.salatreminder.ui.theme.SalatReminderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SalatReminderTheme {
+
+            }
+        }
+    }
+}
